7cc8b969aa3bc76c94338239cd12c374ed4bd988
Abraham Samma <sammadiscerning@gmail.com>
2020-10-18 21:45:18 +0300

    Add split test clicky script

--- src/components/ClickyAnalytics.js
+++ src/components/ClickyAnalytics.js
@@ -3,6 +3,9 @@ import React from 'react';
 const ClickyAnalytics = () => {
   return (
     <>
+      <script>
+        var clicky_custom = clicky_custom || {}; clicky_custom.split = { name: 'landing page main branch', version: 'v0.9.0' };
+      </script>
       <script>var clicky_site_ids = clicky_site_ids || []; clicky_site_ids.push(101280810);</script>
       <script async src="//static.getclicky.com/js" />
       <noscript>
