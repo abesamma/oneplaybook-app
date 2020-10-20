cae156301e0e4cb29bfc807e82a4c0d87275f649
Abraham Samma <sammadiscerning@gmail.com>
2020-10-20 13:14:08 +0300

    Set attribute crossorigin to anonymous for analytics script

--- src/components/ClickyAnalytics.js
+++ src/components/ClickyAnalytics.js
@@ -3,9 +3,9 @@ import React from 'react';
 const ClickyAnalytics = () => {
   return (
     <>
-      <script src="../scripts/clicky-split-test-script.js" />
+      <script src="../scripts/clicky-split-test-script.js" crossorigin="anonymous" />
       <script>var clicky_site_ids = clicky_site_ids || []; clicky_site_ids.push(101280810);</script>
-      <script async src="//static.getclicky.com/js" />
+      <script async src="//static.getclicky.com/js" crossorigin="anonymous" />
       <noscript>
         <p>
           <img alt="Clicky" width="1" height="1" src="//in.getclicky.com/101280810ns.gif" />
