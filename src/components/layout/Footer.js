f9a26d2c2137936621d76bd9320b0a6e407627fe
Abraham Samma <sammadiscerning@gmail.com>
2020-11-15 00:33:29 +0300

    Change footer about to match above fold UVP

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -8,7 +8,7 @@ const Footer = () => {
           <div className="flex-1 px-3">
             <h2 className="text-lg font-semibold">About</h2>
             <p className="mt-5">
-              Oneplaybook app helps you learn, think and build awesome things with anyone easily.
+              Oneplaybook app helps teams can think & get awesome stuff done together.
             </p>
             <p className="mt-2">
               Made with{' '}
