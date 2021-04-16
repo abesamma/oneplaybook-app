b017e49ce4221a3d1f44ed3e557ae236373e9b27
A.B. Samma <sammadiscerning@gmail.com>
2021-04-16 17:04:44 +0300

    Update Footer.js

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -8,7 +8,7 @@ const Footer = () => {
           <div className="flex-1 px-3">
             <h2 className="text-lg font-semibold">About</h2>
             <p className="mt-5">
-              We are helping to make knowledge more accessible for people, teams & organizations via awesome wikis. 🔥
+              We are helping to make knowledge more accessible for people, teams & organizations with awesome wikis. 🔥
             </p>
             <p className="mt-2">
               Made with{' '}
