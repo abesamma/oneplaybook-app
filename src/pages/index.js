689bd66390335cefb6dbe60c908378a1e2937b8a
A.B. Samma <sammadiscerning@gmail.com>
2021-05-06 14:11:45 +0300

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -36,9 +36,9 @@ export default () => (
             </p>
             <p className="font-body text-md md:text-lg mt-4">
               <span className="primary-text-bg font-bold">Oneplaybook</span> helps you build
-              federated internal wiki collections called &apos;playbooks&apos; to use anywhere,
-              locally or on any preferred cloud service, so you can work on the knowledge better
-              with others in one unified space. All data stays with you.
+              internal wiki collections called &apos;playbooks&apos that connect
+              your knowledge with others in unified, federated spaces for shared knowledge,
+              docs, and notes. Use offline or on any preferred cloud service.
             </p>
             <div className="mt-6 md:mt-4">
               <CtaButton className="primary font-body font-bold" size="lg">
