622d850adbe0dbac3548c778144159537ab482c3
Abraham Samma <sammadiscerning@gmail.com>
2021-10-03 23:27:53 +0300

    Add swipeable drawer in WikiView.js

--- src/components/Views/WikiView.js
+++ src/components/Views/WikiView.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import AppBar from '@material-ui/core/AppBar';
-import Drawer from '@material-ui/core/Drawer';
+import Collapse from '@material-ui/core/Collapse';
+import Divider from '@material-ui/core/Divider';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import SwipeableDrawer from '@material-ui/core/SwipeableDrawer';
+import Typography from '@material-ui/core/Typography';
 import IconButton from '@material-ui/core/IconButton';
 import ArrowBackOutlinedIcon from '@material-ui/icons/ArrowBackOutlined';
+import LibraryBooksOutlinedIcon from '@material-ui/icons/LibraryBooksOutlined';
+import BookOutlinedIcon from '@material-ui/icons/BookOutlined';
+import ExpandLess from '@material-ui/icons/ExpandLess';
+import ExpandMore from '@material-ui/icons/ExpandMore';
 import MenuOutlinedIcon from '@material-ui/icons/MenuOutlined';
 import Toolbar from '@material-ui/core/Toolbar';
 import { useMediaQuery } from '@material-ui/core';
@@ -20,7 +31,16 @@ const useStyles = makeStyles(theme => ({
     marginLeft: theme.spacing(1)
   },
   drawer: {
-    width: 200
+    width: 250
+  },
+  list: {
+    width: '100%'
+  },
+  nested: {
+    paddingLeft: theme.spacing(20)
+  },
+  title: {
+    paddingLeft: theme.spacing(8)
   }
 }));
 
@@ -28,7 +48,11 @@ const WikiView = ({ closeWikiHandler }) => {
   const classes = useStyles();
   const mediaQuery = useMediaQuery('(min-width: 1280px)');
   const [drawer, setDrawer] = React.useState(false);
+  const [expand, setExpand] = React.useState(false);
   const toggleDrawer = () => setDrawer(!drawer);
+  const toggleExpand = () => setExpand(!expand);
+  const handleOpenDrawer = () => setDrawer(true);
+  const handleCloseDrawer = () => setDrawer(false);
   return (
     <>
       <AppBar className={classes.appBar} elevation={0} position="fixed">
@@ -48,15 +72,39 @@ const WikiView = ({ closeWikiHandler }) => {
         </Toolbar>
       </AppBar>
       <iframe title="Wiki" className="w-full h-full border-0" src="https://tiddlywiki.com" />
-      <Drawer
+      <SwipeableDrawer
         anchor="left"
         className={classes.drawer}
+        classes={{ paper: classes.drawer }}
         open={drawer}
-        onClose={toggleDrawer}
+        onClose={handleCloseDrawer}
+        onOpen={handleOpenDrawer}
         variant="temporary"
       >
-        Drawer contents
-      </Drawer>
+        <Typography className={classes.title} color="secondary" variant="h6">
+          Wiki title
+        </Typography>
+        <Divider />
+        <List className={classes.list}>
+          <ListItem button onClick={toggleExpand}>
+            <ListItemIcon>
+              <LibraryBooksOutlinedIcon />
+            </ListItemIcon>
+            <ListItemText primary="Library" />
+            {expand ? <ExpandLess /> : <ExpandMore />}
+          </ListItem>
+          <Collapse in={expand} timeout="auto" unmountOnExit>
+            <List component="div" disablePadding>
+              <ListItem button className={classes.nested}>
+                <ListItemIcon>
+                  <BookOutlinedIcon />
+                </ListItemIcon>
+                <ListItemText primary="Another wiki" />
+              </ListItem>
+            </List>
+          </Collapse>
+        </List>
+      </SwipeableDrawer>
     </>
   );
 };
